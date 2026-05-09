f500ade98d5764f2dff808b875e19b27a118c120
JCBBBBBB <wjdcksqls1@naver.com>
2026-05-09 15:53:57 +0900

    feat: supplemental_page_table 초기화 로직 추가

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -173,7 +173,8 @@ vm_do_claim_page (struct page *page) {
 
 /* Initialize new supplemental page table */
 void
-supplemental_page_table_init (struct supplemental_page_table *spt UNUSED) {
+supplemental_page_table_init (struct supplemental_page_table *spt) {
+	hash_init(&spt->page, &spt->page.hash, &spt->page.less, &spt->page.aux);
 }
 
 /* Copy supplemental page table from src to dst */
