c2ac4ca964a152c4ad868a696a58aeedacb83317
binny <binny0x00@gmail.com>
2026-05-14 21:42:48 +0900

    fix: lazy loading page 등록 실패 시 파일과 aux 정리
    vm_alloc_page_with_initializer 호출이 실패하면
    열어 둔 파일을 닫고 aux 메모리를 해제하도록 정리한다

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -1271,9 +1271,11 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 		aux->page_read_bytes = page_read_bytes;
 		aux->page_zero_bytes = page_zero_bytes;
 
-		if (!vm_alloc_page_with_initializer (VM_ANON, upage, writable,
-		                                     lazy_load_segment, aux))
+		if (!vm_alloc_page_with_initializer (VM_ANON, upage, writable, lazy_load_segment, aux)) {
+			file_close (aux->file);
+			free (aux);
 			return false;
+		}
 
 		/*
 		 * 다음 페이지로 진행한다.
