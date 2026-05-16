39ef6eb0844116b0446acdad7bd61c364e413d8b
binny <binny0x00@gmail.com>
2026-05-16 14:00:06 +0900

    feat: page fault 처리에서 stack growth과 페이지 claim을 연결

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -220,13 +220,20 @@ vm_handle_wp (struct page *page UNUSED) {
 
 /* Return true on success */
 bool
-vm_try_handle_fault (struct intr_frame *f UNUSED, void *addr UNUSED,
-		bool user UNUSED, bool write UNUSED, bool not_present UNUSED) {
-	struct supplemental_page_table *spt UNUSED = &thread_current ()->spt;
+vm_try_handle_fault (struct intr_frame *f, void *addr,
+		bool user, bool write UNUSED, bool not_present UNUSED) {
+	struct supplemental_page_table *spt = &thread_current ()->spt;
 	struct page *page = NULL;
 	/* TODO: Validate the fault */
 	/* TODO: Your code goes here */
-
+	if(addr == NULL || !user || not_present){
+		return false;
+	}
+	page = spt_find_page(spt, pg_round_down(addr));
+	if (page == NULL){
+		vm_stack_growth(addr);
+		return true;
+	}
 	return vm_do_claim_page (page);
 }
 
