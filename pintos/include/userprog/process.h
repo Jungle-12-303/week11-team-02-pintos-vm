ed1821f127dd2044ef75876d87238533afaf0dd8
binny <binny0x00@gmail.com>
2026-05-20 01:57:05 +0900

    fix: process 헤더의 off_t include 경로

--- pintos/include/userprog/process.h
+++ pintos/include/userprog/process.h
@@ -2,7 +2,7 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
-#include "off_t.h"
+#include "filesys/off_t.h"
 
 tid_t process_create_initd (const char *file_name);
 tid_t process_fork (const char *name, struct intr_frame *if_);
