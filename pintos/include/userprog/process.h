53216605485851c877af3c03e54139907d54c126
binny <binny0x00@gmail.com>
2026-05-19 20:26:48 +0900

    feat: 종료 시 uninit 페이지 보조 자원 정리

--- pintos/include/userprog/process.h
+++ pintos/include/userprog/process.h
@@ -15,5 +15,12 @@ int process_add_file (struct file *f);
 struct file *process_get_file (int fd);
 void process_close_file (int fd);
 
+struct segment_load_aux {
+	struct file *file;
+	off_t ofs;
+	uint32_t read_bytes;
+	uint32_t zero_bytes;
+};
+
 struct segment_load_aux * copy_segment_load_aux (const struct segment_load_aux *src);
 #endif /* userprog/process.h */
